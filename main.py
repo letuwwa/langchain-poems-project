b4fa1752fad8d20ba6e6746b6e7d918064d9dd9d
Max Litvin <letuwwa@gmail.com>
2026-09-07 23:16:03 +0300

    feat: simple chain

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import typer
 from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 
-def main():
+def main(
+    topic: str = typer.Option(help="Poem topic."),
+    lines: str = typer.Option(5, min=1, help="Maximum amount of lines."),
+):
+    prompt = ChatPromptTemplate.from_template(
+        "Write a simple poem about {topic} in at most {lines} lines. "
+        "Return only the poem."
+    )
     model = ChatOllama(model="gemma2:9b")
-    response = model.invoke("Write a simple poem of up to 5 lines. Return only the poem.")
-    typer.echo(response.content)
+    chain = prompt | model | StrOutputParser()
+
+    poem = chain.invoke({"topic": topic, "lines": lines})
+    typer.echo(poem)
 
 
 if __name__ == "__main__":
