94486ac55d3db4f84a6929dc85e44b99269e8586
Max Litvin <letuwwa@gmail.com>
2026-09-07 22:54:30 +0300

    feat: basic features and model test

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import typer
+from langchain_ollama import ChatOllama
+
+
 def main():
-    print("Hello from langchain-project!")
+    model = ChatOllama(model="gemma2:9b")
+    response = model.invoke("Write a simple poem of up to 5 lines. Return only the poem.")
+    typer.echo(response.content)
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
